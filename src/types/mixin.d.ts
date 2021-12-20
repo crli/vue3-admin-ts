444a5254c6e49f9013ab46d8322624937ee53418
lining <695922129@qq.com>
2021-12-20 13:32:19 +0800

    a

---
+++ src/types/mixin.d.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: crli
+ * @Date: 2021-12-20 13:21:34
+ * @LastEditors: crli
+ * @LastEditTime: 2021-12-20 13:22:36
+ * @Description: file content
+ */
+/* xxxxMixin.js声明文件,如果使用到了mixin里的方法请在此处进行配置声明*/
+// data
+declare var formRulesMixin: Object<any>
